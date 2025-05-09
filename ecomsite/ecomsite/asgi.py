f4148fbb0ec7a5dda59c7c436a0bb8e30be3b22a
Atifsalehin731 <99961478+Atifsalehin731@users.noreply.github.com>
2025-05-09 23:44:32 +0600

    Fixed asgi.py error

--- ecomsite/ecomsite/asgi.py
+++ ecomsite/ecomsite/asgi.py
@@ -1,6 +1,10 @@
 """
 ASGI config for ecomsite project.
 
+ASGI config for ecomsite project.
+
+ASGI config for ecomsite project.
+
 It exposes the ASGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
