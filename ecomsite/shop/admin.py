fc53dc480a98e5da7df160e0a530ae358b0ae122
Atifsalehin731 <99961478+Atifsalehin731@users.noreply.github.com>
2025-05-09 23:22:36 +0600

    Update admin.py

--- ecomsite/shop/admin.py
+++ ecomsite/shop/admin.py
@@ -7,3 +7,15 @@ from .models import Order
 
 admin.site.register(Product)
 admin.site.register(Order)
+
+
+#from django.contrib import admin
+#from .models import Product
+#from .models import Order
+#from django.contrib import admin
+#from django.contrib import admin
+#from .models import Product
+#from .models import Order
+#from .models import Product
+#from .models import Order
+
