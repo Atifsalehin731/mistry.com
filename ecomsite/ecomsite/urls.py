f54546c4b45c6754efbfb67731a6a01f16e596d6
Atifsalehin731 <99961478+Atifsalehin731@users.noreply.github.com>
2025-05-09 23:43:46 +0600

    Update urls.py

--- ecomsite/ecomsite/urls.py
+++ ecomsite/ecomsite/urls.py
@@ -1,5 +1,6 @@
 """
 URL configuration for ecomsite project.
+URL configuration for ecomsite project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.0/topics/http/urls/
